70dfd83bfdfdad707361306890a5ea8597d08528
EvyaB <evyatarbittonwork@gmail.com>
2022-03-10 12:40:46 +0200

    Added unit tests for basic Cell class and for the Gameboard Repository class

---
+++ UnitTests/CellTestSuit.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SnakesAndLadderEvyatar.Models;
+using Xunit;
+
+namespace UnitTests
+{
+    public class CellTestSuit
+    {
+        [Theory]
+        [InlineData(1, 1, 1, 1, true)]
+        [InlineData(0, 0, 0, 0, true)]
+        [InlineData(15, 15, 15, 15, true)]
+        [InlineData(1, 1, 1, 2, false)]
+        [InlineData(1, 1, 2, 1, false)]
+        [InlineData(1, 1, 2, 2, false)]
+        [InlineData(1, 2, 1, 1, false)]
+        [InlineData(2, 1, 1, 1, false)]
+        [InlineData(2, 2, 1, 1, false)]
+        public void CellCompareEqualTest(int row1, int column1, int row2, int column2, bool result)
+        {
+            Cell c1 = new Cell(row1, column1);
+            Cell c2 = new Cell(row2, column2);
+            
+            Assert.Equal(result, c1 == c2);
+            Assert.Equal(result, c1.Equals(c2));
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1, 1, false)]
+        [InlineData(0, 0, 0, 0, false)]
+        [InlineData(15, 15, 15, 15, false)]
+        [InlineData(1, 1, 1, 2, true)]
+        [InlineData(1, 1, 2, 1, true)]
+        [InlineData(1, 1, 2, 2, true)]
+        [InlineData(1, 2, 1, 1, true)]
+        [InlineData(2, 1, 1, 1, true)]
+        [InlineData(2, 2, 1, 1, true)]
+        public void CellCompareUnequalsTest(int row1, int column1, int row2, int column2, bool result)
+        {
+            Cell c1 = new Cell(row1, column1);
+            Cell c2 = new Cell(row2, column2);
+
+            Assert.Equal(result, c1 != c2);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1, 1, 0)]
+        [InlineData(0, 0, 0, 0, 0)]
+        [InlineData(15, 15, 15, 15, 0)]
+        [InlineData(1, 1, 1, 2, -1)]
+        [InlineData(1, 1, 2, 1, -1)]
+        [InlineData(1, 1, 2, 2, -1)]
+        [InlineData(2, 5, 3, 1, -1)]
+        [InlineData(1, 2, 1, 1, 1)]
+        [InlineData(2, 1, 1, 1, 1)]
+        [InlineData(2, 2, 1, 1, 1)]
+        [InlineData(3, 2, 2, 5, 1)]
+        public void CellCompareToTest(int row1, int column1, int row2, int column2, int result)
+        {
+            Cell c1 = new Cell(row1, column1);
+            Cell c2 = new Cell(row2, column2);
+            
+            Assert.Equal(result, c1.CompareTo(c2));
+        }
+    }
+}
---
+++ UnitTests/GameboardRepositoryTestSuit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Moq.EntityFrameworkCore;
+using SnakesAndLadderEvyatar.Models;
+using SnakesAndLadderEvyatar.Repositories;
+using Xunit;
+
+namespace UnitTests
+{
+    // TODO: Update these tests once the Gameboard is not hardcoded in the Gameboard class
+    public class GameboardRepositoryTestSuit
+    {
+        private readonly GameboardRepository _gameboardRepository;
+        public GameboardRepositoryTestSuit()
+        {
+            _gameboardRepository = new GameboardRepository();
+        }
+
+        [Fact]
+        public void GetBoardColumnsCountTest()
+        {
+            // Currently the game board is hardcoded in the Gameboard class and is always the same size.
+            Assert.Equal(6, _gameboardRepository.GetBoardColumnsCount());
+        }
+        [Fact]
+        public void GetBoardRowsCountTest()
+        {
+            // Currently the game board is hardcoded in the Gameboard class and is always the same size.
+            Assert.Equal(6, _gameboardRepository.GetBoardRowsCount());
+        }
+
+        [Fact]
+        public void GetFinalCellTest()
+        {
+            Cell finalCell = _gameboardRepository.GetFinalCell();
+            int rowsCount = _gameboardRepository.GetBoardRowsCount();
+            int columnsCount = _gameboardRepository.GetBoardColumnsCount();
+            Assert.Equal(rowsCount, finalCell.Row);
+            Assert.Equal(columnsCount, finalCell.Column);
+        }
+
+        [Theory]
+        [InlineData(1, 1, false, null)]
+        [InlineData(3, 5, false, null)]
+        [InlineData(0, 0, false, null)]
+        [InlineData(-1, 5, false, null)]
+        [InlineData(1, -5, false, null)]
+        [InlineData(-1, -5, false, null)]
+        [InlineData(3, 6, true, typeof(LadderCell))]
+        [InlineData(5, 5, true, typeof(SnakeCell))]
+        public void GetCellModifierTest(int row, int column, bool expectingCellToHaveModifier, Type expectedModifierType)
+        {
+            Cell cell = new Cell(row, column);
+            bool hasModifier = _gameboardRepository.GetCellModifier(cell, out var cellModifier);
+
+            Assert.Equal(expectingCellToHaveModifier, hasModifier);
+
+            if (expectingCellToHaveModifier)
+            {
+                Assert.IsType(expectedModifierType, cellModifier);
+            }
+            else
+            {
+                Assert.Null(cellModifier);
+            }
+        }
+    }
+}
